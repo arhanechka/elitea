3400aab886dac6e7f13297655a75dc0823de6d4a
arhanechka <arhanechka@gmail.com>
2026-03-23 09:42:43 +0000

    Add test steps

--- tests/generated_test.spec.ts
+++ tests/generated_test.spec.ts
@@ -1,5 +1,8 @@
 import { test, expect } from '@playwright/test';
 
 test('EPAM Client Work page test', async ({ page }) => {
-  /* STEPS_PLACEHOLDER */
+  await page.goto('https://www.epam.com/');
+  await page.click('text=Services');
+  await page.click('text=Explore Our Client Work');
+  await expect(page.locator('text=Client Work')).toBeVisible();
 });
